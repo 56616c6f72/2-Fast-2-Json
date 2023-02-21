2f446eec067e08273967d4f3f825b7a64eda467b
56616c6f72 <>
2023-02-21 17:18:22 +0000

    Speed: multithreading

--- src/lib.rs
+++ src/lib.rs
@@ -22,21 +22,25 @@ pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
 
     let mut file_buffer = BufWriter::with_capacity(26214400,file);
   
+    let thread_pool = rayon::ThreadPoolBuilder::new()
+        .num_threads(256)
+        .build()
+        .unwrap();
 
     let mut it = rdr.into_records();
     let headers = it.next().unwrap().unwrap();
-    let mut json_map = Map::new();
-    
+    let mut json_map = Map::with_capacity(headers.len()); 
+ 
+    thread_pool.install(|| {
     for line in it {
-          // let mut json_map = Map::with_capacity(headers.len()); 
 
         for (i, value)in line.unwrap().iter().enumerate(){
-               // println!("{:?}", value);
                 json_map.insert(headers.get(i).unwrap().to_string(), serde_json::from_str(value).unwrap_or_else(|_|value.into()));
             }
-        
         writeln!(file_buffer,"{}", serde_json::to_string(&json_map).unwrap()).expect("Buffer store no good.");
-    };
+        json_map.clear();
+      };
+    });
 
     file_buffer.flush().expect("buffer write no good.");
     Ok(())
