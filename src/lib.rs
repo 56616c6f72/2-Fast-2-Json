6ee84a71a2d514afad0e66d397b92c36e35e8460
56616c6f72 <>
2023-04-13 13:08:12 +0100

    Improving error handling

--- src/lib.rs
+++ src/lib.rs
@@ -1,17 +1,17 @@
-use std::{error::Error, io::{BufWriter, Write}};
+use std::io::{BufWriter, Write};
 use csv::ReaderBuilder;
 use serde_json::Map;
 use std::fs::OpenOptions;
+use anyhow::{Context, Result};
 
-
-pub fn run(sfile: String,ofile: String,deli: String,tab_deli: bool) -> Result<(), Box<dyn Error>> {
+pub fn run(sfile: String,ofile: String,deli: String,tab_deli: bool) -> Result<(), anyhow::Error> {
     let tmp_delimiter: u8 = if tab_deli {
         b'\t'
     } else {
         deli.as_bytes()[0]
     };
 
-    let rdr = ReaderBuilder::new()
+    let csv_reader = ReaderBuilder::new()
     .flexible(true)
     .has_headers(false)
     .delimiter(tmp_delimiter)
@@ -24,26 +24,26 @@ pub fn run(sfile: String,ofile: String,deli: String,tab_deli: bool) -> Result<()
         .open(ofile)?;
 
     let mut file_buffer = BufWriter::with_capacity(26214400,file);
-  
-    // let thread_pool = rayon::ThreadPoolBuilder::new()
-    //     .build()
-    //     .unwrap();
+    let mut csv_iterator = csv_reader.into_records();
+    
+    let headers = match csv_iterator.next() {
+                                    Some(record) => match record { Ok(value) => value, _ => anyhow::bail!("Failed to read the first StringRecord(header)!")},
+                                    _ =>    anyhow::bail!("Failed to read the CSV iterator!"),
+                                };
 
-    let mut it = rdr.into_records();
-    let headers = it.next().unwrap().unwrap();
     let mut json_map = Map::with_capacity(headers.len()); 
  
-    for line in it {
-        for (i, value)in line.unwrap().iter().enumerate(){
-                if !headers.get(i).unwrap().to_string().is_empty(){
-                    json_map.insert(headers.get(i).unwrap().to_string(), serde_json::from_str(value).unwrap_or_else(|_|value.into()));
+    for line in csv_iterator {
+        for (i, value)in line.context("Failed to iterate over csv lines!")?.iter().enumerate(){
+                if !headers.get(i).with_context(|| format!("Failed to read header index {}!", i))?.to_string().is_empty(){
+                    json_map.insert(headers.get(i).with_context(|| format!("Failed to read header index {}!", i))?.to_string(), serde_json::from_str(value).unwrap_or_else(|_|value.into()));
                 }
             }
-        writeln!(file_buffer,"{}", serde_json::to_string(&json_map).unwrap()).expect("Buffer store no good.");
+        writeln!(file_buffer,"{}", serde_json::to_string(&json_map).context("Failed to convert json to string before writting to buffer.")?).context("Writing to file_buffer failed!")?;
         json_map.clear();
     };
 
 
-    file_buffer.flush().expect("buffer write no good.");
+    file_buffer.flush().context("Flushing file_buffer to disk failed!")?;
     Ok(())
 }
